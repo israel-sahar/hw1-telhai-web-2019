e2a98030020c65f58e977397e1f2c4198f66365f
israel-sahar <37867238+israel-sahar@users.noreply.github.com>
2019-04-10 18:36:43 +0300

    .
    #6 #5

--- res/scripts.js
+++ res/scripts.js
@@ -1,9 +1,9 @@
-                var counter = 0;
-                function changeImage(source) {
-                    document.getElementById('mainPic').src = source.src;
-                }
-        
-                function UpByLike() {
-                    counter++;
-                    document.getElementById('counter').innerHTML = counter;
-                }
+function UpByLike() {
+    counter++;
+    document.getElementById('counter').innerHTML = counter;
+}
+
+var counter = 0;
+function changeImage(source) {
+    document.getElementById('main-photo').src = source.src;
+}
